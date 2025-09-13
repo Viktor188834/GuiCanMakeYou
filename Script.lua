b3406550b30271348b68e9c221078fc24dd1abaf
Viktor188834 <atabaevviktor671@gmail.com>
2025-09-13 17:47:16 +0300

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -341,9 +341,3 @@ end, "Test")]]
 --например телепортация: character.HumanoidRootPart.CFrame = CFrame.new(0, 100, 0)
 --например изменение скорости: character.Humanoid.WalkSpeed = 100
 --и есть телепортация выше себя на 100 студов: character.HumanoidRootPart.CFrame = character.HumanoidRootPart.CFrame * CFrame.new(0, 100, 0)
-
-AddSection("--Teleportings--")
-
-AddClickButton("Teleport Up You 100 Studs", function()
-	character.HumanoidRootPart.CFrame = character.HumanoidRootPart.CFrame * CFrame.new(0, 100, 0)
-end, "Teleport Up Your'e character 100 Studs")
